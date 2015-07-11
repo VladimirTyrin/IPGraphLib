439e2e20de2e2334c6c9f2e9f90df87e3dfccbe8
Vladimir Tyrin <tyrin.v.s@itcc.company>
2015-07-11 20:37:37 +0300

    start using xlib

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,52 @@
 #include <vector>
+#include <stdio.h>
 
 #include "GraphLib.h"
 
 using std::vector;
+
+enum EXIT_STATUS : int
+{
+  OK = 0,
+  ERROR = 1
+};
+
+static Display *display;
+static Window window;
+static GC gc;
+
+
+EXIT_STATUS initDisplay(int XRES, int YRES)
+{
+  display = XOpenDisplay( NULL );
+  if (! display)
+    return ERROR;
+
+  int screen = DefaultScreen(display);
+  unsigned int white = WhitePixel(display, screen);
+  unsigned int black = BlackPixel(display, screen);
+
+  window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, XRES, YRES, 0, black, white);
+
+  Atom wmDelete=XInternAtom(display, "WM_DELETE_WINDOW", True);
+  XSetWMProtocols(display, window, &wmDelete, 1);
+
+  gc = XCreateGC(display, window, 0, NULL);
+
+  XSetForeground(display, gc, black);
+  XMapWindow(display, window);
+  XClearWindow(display, window);
+  sleep(5);
+  return OK;
+}
+
+
+int finishDisplay()
+{
+  XDestroyWindow(display, window);
+  XCloseDisplay(display);
+  return OK;
+}
 
 
 int main()
@@ -23,8 +67,12 @@ int main()
   for (auto element: shapeVector)
     element->printInfo();
 
+  if (initDisplay(500, 500) != OK)
+    return 1;
+  sleep(5);
   for (auto element: shapeVector)
     element->draw();
+  finishDisplay();
 
   return 0;
 }
